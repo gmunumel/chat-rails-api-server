3cb1ba6c4e43751bdb7bb7023adcb684b53a9372
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-06-08 19:29:39 +0200

    Fixed config redis file

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -7,4 +7,4 @@ $redis = Redis.new(cnfg)
 #$redis_ns = Redis::Namespace.new(cnfg[:namespace], :redis => $redis) if cnfg[:namespace]
 
 # To clear out the db before each test
-$redis.flushdb if Rails.env = "test"
+$redis.flushdb if Rails.env == "test"
