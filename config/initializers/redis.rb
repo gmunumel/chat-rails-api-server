1994b9cbe2e0f14abcece72f693bf22ced10e016
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-06-10 13:15:15 +0200

    Fixed issue to load redis_url config value

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,13 @@
 #config/initializers/redis.rb
 REDIS_CONFIG = YAML.load( File.open( Rails.root.join("config/redis.yml") ) ).symbolize_keys
 dflt = REDIS_CONFIG[:default].symbolize_keys
-cnfg = REDIS_CONFIG[Rails.env.to_sym] ? REDIS_CONFIG[Rails.env.to_sym].symbolize_keys : dflt
+if (REDIS_CONFIG[Rails.env.to_sym])
+  host = eval(REDIS_CONFIG[Rails.env.to_sym]["host"])
+  REDIS_CONFIG[Rails.env.to_sym]["host"] = host
+  cnfg = REDIS_CONFIG[Rails.env.to_sym].symbolize_keys
+else
+  cnfg = dflt
+end
 
 $redis = Redis.new(cnfg)
 #$redis_ns = Redis::Namespace.new(cnfg[:namespace], :redis => $redis) if cnfg[:namespace]
