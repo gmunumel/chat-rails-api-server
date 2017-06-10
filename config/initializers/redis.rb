a8523b51622d4e69a5eeebdbb0d644d88bdc102f
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-06-10 18:10:20 +0200

    Fixed a local problem with redis_url config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 #config/initializers/redis.rb
 REDIS_CONFIG = YAML.load( File.open( Rails.root.join("config/redis.yml") ) ).symbolize_keys
 dflt = REDIS_CONFIG[:default].symbolize_keys
-if (REDIS_CONFIG[Rails.env.to_sym])
+if (REDIS_CONFIG[Rails.env.to_sym] && Rails.env == "production")
   url = eval(REDIS_CONFIG[Rails.env.to_sym]["url"])
   REDIS_CONFIG[Rails.env.to_sym]["url"] = url
   cnfg = REDIS_CONFIG[Rails.env.to_sym].symbolize_keys
