8fe8de7a499fc7143f6b5cdcd04b638a7718a3e3
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-06-10 13:22:03 +0200

    Changed from host to url redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,8 +2,8 @@
 REDIS_CONFIG = YAML.load( File.open( Rails.root.join("config/redis.yml") ) ).symbolize_keys
 dflt = REDIS_CONFIG[:default].symbolize_keys
 if (REDIS_CONFIG[Rails.env.to_sym])
-  host = eval(REDIS_CONFIG[Rails.env.to_sym]["host"])
-  REDIS_CONFIG[Rails.env.to_sym]["host"] = host
+  url = eval(REDIS_CONFIG[Rails.env.to_sym]["url"])
+  REDIS_CONFIG[Rails.env.to_sym]["url"] = url
   cnfg = REDIS_CONFIG[Rails.env.to_sym].symbolize_keys
 else
   cnfg = dflt
