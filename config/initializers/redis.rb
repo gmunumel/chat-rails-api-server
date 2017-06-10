062c3da11909ff1bfac596c8d1b71830d8f42611
Gabriel Munumel <gabrielmunumel@gmail.com>
2017-06-10 12:34:34 +0200

    Changed redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 #config/initializers/redis.rb
 REDIS_CONFIG = YAML.load( File.open( Rails.root.join("config/redis.yml") ) ).symbolize_keys
 dflt = REDIS_CONFIG[:default].symbolize_keys
-cnfg = dflt.merge(REDIS_CONFIG[Rails.env.to_sym].symbolize_keys) if REDIS_CONFIG[Rails.env.to_sym]
+cnfg = REDIS_CONFIG[Rails.env.to_sym] ? REDIS_CONFIG[Rails.env.to_sym].symbolize_keys : dflt
 
 $redis = Redis.new(cnfg)
 #$redis_ns = Redis::Namespace.new(cnfg[:namespace], :redis => $redis) if cnfg[:namespace]
